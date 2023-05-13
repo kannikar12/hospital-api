828a802a3c146b9a2f9056f6ab39a30ad7cc7092
kannikar12 <63317001238@rmu.ac.th>
2023-05-13 11:23:25 +0700

    13/5/23/11:23

--- src/main/java/com/it/dto/ProvinceDto.java
+++ src/main/java/com/it/dto/ProvinceDto.java
@@ -1,13 +1,13 @@
 package com.it.dto;
 
-
 import lombok.Data;
 
 @Data
- class ProvinceDto {
+public class ProvinceDto {
+  
 	private Integer pvnId;
+	private String pvnCode;
 	private String pvnNameTh;
 	private String pvnNameEng;
 	private String pvnArea;
-	
 }
