9a83fd66cd828eab35afc018fb3479a1c9e1d7e7
kannikar12 <63317001238@rmu.ac.th>
2023-05-17 16:40:33 +0700

    17/5/23/16:40

--- src/main/java/com/it/repository/BookingRepository.java
+++ src/main/java/com/it/repository/BookingRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.it.entity.BookingEntity;
 
-
-
 public interface BookingRepository extends JpaRepository<BookingEntity,Integer> {
 
 }
